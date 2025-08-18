09228ebca0a50e814e76fe8500d3bcfc6e677e6a
cjgaland <cjgaland@gmail.com>
2025-08-18 12:40:09 +0200

    19

--- Formacion/Sesiones/hasta-2019/data.js
+++ Formacion/Sesiones/hasta-2019/data.js
@@ -1,19 +1,50 @@
 // hasta-2019/data.js
 registerSessions('hasta-2019', [
   {
-    title: "Abordaje precoz de la sepsis en urgencias",
-    author: "Dra. Mariola Granados Roldán",
-    date: "2019-03-10",
-    tags: ["Urgencias","Emergencias","Sepsis"],
-    pdf:  "docs/2019-03-10_sepsis_granados.pdf",
-    pptx: ""
+    title: "ERC 2015",
+    author: "Dr. Carlos J. Galán Doval",
+    date: "2015-10-22",
+    tags: ["Urgencias","Emergencias","Soporte Vital"],
+    pdf:  "docs/2015-10-22_ERC_Carlos.pdf",
   },
   {
-    title: "Manejo inicial del traumatismo craneoencefálico grave",
+    title: "Muerte Súbita Cardiaca",
     author: "Dr. Carlos J. Galán Doval",
-    date: "2019-01-15",
-    tags: ["Traumatología","Neurotrauma"],
-    pdf:  "docs/2019-01-15_tce_galan.pdf",
-    pptx: "docs/2019-01-15_tce_galan.pptx"
+    date: "2018-04-25",
+    tags: ["Muerte Súbita","Emergencias","Cardiaca"],
+    pdf:  "docs/2018-04-25_MuerteSubita_Carlos.pdf",
+  },
+  {
+    title: "Póster Medicación Urgencias",
+    author: "Dr. Carlos J. Galán Doval",
+    date: "2000-08-31",
+    tags: ["Urgencias","Emergencias","Fármacos","Medicación"],
+    pdf:  "docs/2000-08-31 PosterMedicacionUrgencias_Carlos.pdf",
+  },
+
+    {
+    title: "Hiponatremias",
+    author: "Carlos J. Galán Doval",
+    date: "2019-29-10",                 // ¡formato ISO para ordenar!: aaaa-mm-dd
+    tags: ["Urgencias","Hiponatremia","Ateración Metabolica"],
+    attachments: [
+      {
+        label: "PDF",
+        href: "docs/2019-10-29 Hiponatremias.pdf",
+        type: "pdf"
+      },
+      {
+        label: "PPTX",
+        href: "docs/2019-10-29 Hiponatremias.pptx",
+        type: "pptx"
+      },
+      {
+        label: "XLSX (datos)",
+        href: "docs/2019-10-29 Hiponatremias.xls",
+        type: "xls"
+      },
+
+    ]
   }
+  
 ]);
