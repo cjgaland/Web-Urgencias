499628295a643e749c401a2323bd6b98ab4be414
cjgaland <cjgaland@gmail.com>
2025-08-18 12:18:29 +0200

    2020

--- Formacion/Sesiones/2020/data.js
+++ Formacion/Sesiones/2020/data.js
@@ -1,19 +1,4 @@
 // 2020/data.js
 registerSessions('2020', [
-  {
-    title: "Abordaje precoz de la sepsis en urgencias",
-    author: "Dra. Mariola Granados Roldán",
-    date: "2020-03-10",
-    tags: ["Urgencias","Emergencias","Sepsis"],
-    pdf:  "docs/2020-03-10_sepsis_granados.pdf",
-    pptx: ""
-  },
-  {
-    title: "Manejo inicial del traumatismo craneoencefálico grave",
-    author: "Dr. Carlos J. Galán Doval",
-    date: "2020-01-15",
-    tags: ["Traumatología","Neurotrauma"],
-    pdf:  "",
-    pptx: "docs/2020-01-15_tce_galan.pptx"
-  }
+
 ]);
