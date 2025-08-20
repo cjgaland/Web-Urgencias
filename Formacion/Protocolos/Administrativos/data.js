babcfd2431b9d9063b10fe0d757b8652d5ce4afb
cjgaland <cjgaland@gmail.com>
2025-08-20 09:28:33 +0200

    Ad

--- Formacion/Protocolos/Administrativos/data.js
+++ Formacion/Protocolos/Administrativos/data.js
@@ -1,26 +1,31 @@
 // Formacion/Protocolos/Administrativos/data.js
-registerProtocols('ADMIN', [
+registerProtocols('Administrativos', [
   {
     title: "Identificación unívoca de pacientes",
-    created_on: "2023-03-15",
-    created_by: "Unidad de Calidad",
+    date: "2023-03-15",
+    author: "Unidad de Calidad",
     revised_on: "2024-06-01",
     revised_by: "Unidad de Calidad",
     tags: ["Administrativo","Seguridad del paciente","Identificación"],
     attachments: [
-      { label:"PDF", href:"docs/2024-09-15_protocolo-identificacion-univoca_ugc-benameji.pdf", type:"pdf" }
-    ]
-  },
+      { label:"PDF", 
+        href:"docs/2024-09-15_protocolo-identificacion-univoca_ugc-benameji.pdf", 
+        type:"pdf" }
+      ]
+    
+    },
+  
   {
     title: "Respeto a la intimidad y confidencialidad",
-    created_on: "2022-10-05",
-    created_by: "Dirección de Enfermería",
+    date: "2022-10-05",
+    author: "Dirección de Enfermería",
     revised_on: "",
     revised_by: "",
     tags: ["Administrativo","Derechos del paciente"],
     attachments: [
-      { label:"PDF", href:"docs/2024-09-15_plan-intimidad-privacidad-confort_benameji-galan-romero-delgado.pdf", type:"pdf" }
+      { label:"PDF", 
+        href:"docs/2024-09-15_plan-intimidad-privacidad-confort_benameji-galan-romero-delgado.pdf", 
+        type:"pdf" }
     ]
   }
 ]);
-
