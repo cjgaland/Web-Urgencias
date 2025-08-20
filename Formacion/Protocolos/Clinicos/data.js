7a1d851ff86000d640dddea8319b8c7ec481808b
cjgaland <cjgaland@gmail.com>
2025-08-20 09:29:09 +0200

    Cli

--- Formacion/Protocolos/Clinicos/data.js
+++ Formacion/Protocolos/Clinicos/data.js
@@ -1,25 +1,30 @@
 // Formacion/Protocolos/Clinicos/data.js
-registerProtocols('CLINICOS', [
+registerProtocols('Clinicos', [
   {
     title: "Manejo del dolor agudo en Urgencias",
-    created_on: "2024-11-20",
-    created_by: "Equipo de Urgencias",
+    date: "2024-11-20",
+    author: "Equipo de Urgencias",
     revised_on: "2025-02-05",
     revised_by: "Comisión de Calidad",
     tags: ["Clínico","Analgesia","Dolor"],
     attachments: [
-      { label:"PDF", href:"docs/2024-09-15_protocolo-abordaje-dolor_ugc-benameji.pdf", type:"pdf" }
-    ]
+      { label:"PDF", 
+        href:"docs/2024-09-15_protocolo-abordaje-dolor_ugc-benameji.pdf", 
+        type:"pdf" }
+      ]
   },
+  
   {
     title: "Preparación segura de medicación",
-    created_on: "2023-09-10",
-    created_by: "Farmacia / Urgencias",
+    date: "2023-09-10",
+    author: "Farmacia / Urgencias",
     revised_on: "",
     revised_by: "",
     tags: ["Clínico","Seguridad del paciente","Medicación"],
     attachments: [
-      { label:"PDF", href:"docs/Protocolo Preparación de Medicación.pdf", type:"pdf" }
+      { label:"PDF", 
+        href:"docs/Protocolo Preparación de Medicación.pdf", 
+        type:"pdf" }
     ]
   }
 ]);
