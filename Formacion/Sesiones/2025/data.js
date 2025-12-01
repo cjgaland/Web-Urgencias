6328ad2ba60c632596382e6f7db80457ca164a29
cjgaland <cjgaland@gmail.com>
2025-12-01 11:06:53 +0100

    SES2025

--- Formacion/Sesiones/2025/data.js
+++ Formacion/Sesiones/2025/data.js
@@ -1,4 +1,20 @@
 // 2025/data.js
 registerSessions('2025', [
 
+{
+    title: "Actuación en Urgencias ante la Violencia de Género",
+    author: "Esperanza Soto Rueda",
+    date: "2025-12-26",                 // ¡formato ISO para ordenar!: aaaa-mm-dd
+    tags: ["Urgencias","Violencia de Género","Violencia"],
+    attachments: [
+      {
+        label: "PDF",
+        href: "docs/2025-12-26_Actuación en Urgencias ante la violencia de género_Esperanza.pdf",
+        type: "pdf"
+      },
+
+     ]
+  } 
+    
+    
 ]);
